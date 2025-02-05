2093746080a1269e3aa9458fb4b56ae8b99865a8
alireza <farmanialireza668@gmail.com>
2025-02-05 12:59:01 +0330

    create and custom admin for post models

--- shop/blog/admin.py
+++ shop/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import*
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','author','created','status']
+    ordering = ['author','status']
+    list_filter = ['author','created','status']
+    search_fields = ['author','title','slug','created','status']
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    list_editable = ['status','author']
+    list_display_links = ['slug']
